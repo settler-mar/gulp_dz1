5fbc2368431989def8e13428925d09f2d508df59
Max Matiukhin <matuhinmax@mail.ru>
2016-10-03 13:21:17 +0300

    Fix gulp run

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -1,11 +1,5 @@
 'use strict';
 
 module.exports = [
-  './source/js/app.js',
-  './source/js/map.js',
-  './source/js/menu.js',
-  './source/js/jquery.next_item_animation.js',
-  './source/js/scroll_block_menu.js',
-  './source/js/bg_animation.js',
-  './source/js/jquery.slider.js'
+  './source/js/app.js'
 ];
--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,9 +1,5 @@
 'use strict';
 
 module.exports = [
-  './bower_components/jquery/dist/jquery.min.js',
-  './bower_components/video.js/dist/video.min.js',
-  './bower_components/BigVideo/lib/bigvideo.js',
-  './source/js/foundation/pie-chart.js',
-  './bower_components/parallax/deploy/jquery.parallax.min.js'
+  './bower_components/jquery/dist/jquery.min.js'
 ];
